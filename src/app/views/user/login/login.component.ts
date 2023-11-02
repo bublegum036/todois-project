0d97a4dc6a4c9deb488bb8c86e1e655d899b14cc
bublegum036 <118725827+bublegum036@users.noreply.github.com>
2023-11-02 18:10:28 +0300

    signup page

--- src/app/views/user/login/login.component.ts
+++ src/app/views/user/login/login.component.ts
@@ -1,8 +1,8 @@
-import { Component } from '@angular/core';
-import { InputTextModule } from 'primeng/inputtext';
-import { PasswordModule } from 'primeng/password';
-import { ButtonModule } from 'primeng/button';
-import {FormControl, Validators} from "@angular/forms";
+import {Component} from '@angular/core';
+import {InputTextModule} from 'primeng/inputtext';
+import {PasswordModule} from 'primeng/password';
+import {ButtonModule} from 'primeng/button';
+import {Validators} from "@angular/forms";
 import {FormBuilder} from "@angular/forms";
 
 @Component({
@@ -19,15 +19,4 @@ export class LoginComponent {
     userName: ['', [Validators.required, Validators.pattern('^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$')]],
     password: ['', [Validators.required]]
   })
-
-  resetClasses(control: FormControl): void {
-    if (control.invalid && (control.dirty || control.touched)) {
-      control.reset();
-    }
-  }
-
-  isInvalid(control: FormControl): boolean {
-    return control.invalid && (control.dirty || control.touched) ;
-  }
-
 }
--- src/app/views/user/signup/signup.component.ts
+++ src/app/views/user/signup/signup.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import {FormBuilder, Validators} from "@angular/forms";
+import { InputTextModule } from 'primeng/inputtext';
+import { PasswordModule } from 'primeng/password';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'signup',
@@ -7,4 +11,13 @@ import { Component } from '@angular/core';
 })
 export class SignupComponent {
 
+  constructor(private fb: FormBuilder) {
+  }
+
+  signupForm = this.fb.group({
+    firstName: ['', [Validators.required, Validators.pattern('/^[а-яА-Яa-zA-Z\\s]+$/')]],
+    email: ['', [Validators.required, Validators.pattern('^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$')]],
+    password: ['', [Validators.required, Validators.pattern(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/)]],
+    passwordRepeat: ['', [Validators.required, Validators.pattern(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/)]]
+  })
 }
