42b73d959a6c9ac82f77ad082fce317808a05efb
bublegum036 <118725827+bublegum036@users.noreply.github.com>
2023-11-08 19:55:28 +0300

    task-form form-control

--- src/app/shared/components/task-form/task-form.component.ts
+++ src/app/shared/components/task-form/task-form.component.ts
@@ -15,9 +15,9 @@ export class TaskFormComponent implements OnInit {
   constructor(private fb: FormBuilder) {
   }
 
-  taskForm= this.fb.group({
-    taskName: ['', [Validators.required, Validators.pattern('^[а-яА-Яa-zA-Z0-9\\s\\p{P}]+$')]],
-    taskDescription: ['', [Validators.required, Validators.pattern('^[а-яА-Яa-zA-Z0-9\\s\\p{P}]+$')]],
+  taskForm = this.fb.group({
+    taskName: ['', [Validators.required, Validators.maxLength(30), Validators.pattern('^[а-яА-Яa-zA-Z0-9\\s\\p{P}]+$')]],
+    taskDescription: ['', [Validators.required, Validators.maxLength(256), Validators.pattern('^[а-яА-Яa-zA-Z0-9\\s\\p{P}]+$')]],
     taskDateSet: ['', [Validators.required]],
     taskDeadline: ['', [Validators.required]],
     taskPriority: ['', [Validators.required]],
@@ -25,24 +25,27 @@ export class TaskFormComponent implements OnInit {
   })
 
 
-
   ngOnInit() {
     this.priority = [
       {
         label: 'Высокий',
+        data: 'Высокий',
         icon: 'pi pi-angle-double-up'
       },
       {
         label: 'Обычный',
+        data: 'Обычный',
         icon: 'pi pi-angle-up'
 
       },
       {
         label: 'Низкий',
+        data: 'Низкий',
         icon: 'pi pi-arrow-right'
       },
       {
         label: 'Очень низкий',
+        data: 'Очень низкий',
         icon: 'pi pi-arrow-down-right'
       },
     ]
@@ -71,11 +74,27 @@ export class TaskFormComponent implements OnInit {
 
 
   createTask() {
-    console.log(this.taskForm.value.taskName)
-    console.log(this.taskForm.value.taskDescription)
-    console.log(this.taskForm.value.taskDateSet)
-    console.log(this.taskForm.value.taskDeadline)
-    console.log(this.taskForm.value.taskPriority)
-    console.log(this.taskForm.value.taskCategory)
+    // if (this.taskForm.valid && this.taskForm.value.taskName
+    //   && this.taskForm.value.taskDescription
+    //   && this.taskForm.value.taskDateSet
+    //   && this.taskForm.value.taskDeadline
+    //   && this.taskForm.value.taskPriority
+    //   && this.taskForm.value.taskCategory) {
+
+      // const task: TaskType = {
+      //   taskName: this.taskForm.value.taskName,
+      //   taskDescription: this.taskForm.value.taskName,
+      //   taskDateSet: this.taskForm.value.taskDateSet,
+      //   taskDeadline: this.taskForm.value.taskDeadline,
+      //   taskPriority: this.taskForm.value.taskPriority,
+      //   taskCategory: this.taskForm.value.taskCategory
+      // }
+
+      console.log(typeof this.taskForm.value.taskPriority)
+      console.log(this.taskForm.value.taskPriority)
+      console.log(this.taskForm.value.taskPriority)
+      console.log(typeof this.taskForm.value.taskDateSet)
+      console.log(this.taskForm.value.taskDateSet)
+    // }
   }
 }
